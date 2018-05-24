004e8e16e339d6ab8dde266de354612a11135331
Phuong Tran Ha <thaphuong@tma.com.vn>
2018-05-24 17:35:56 +0700

    edit 24/05

--- js/re-home.js
+++ js/re-home.js
@@ -1,1 +1,9 @@
-var offset =300, offsetOpacity = 1200, scrollDuration = 700;
+window.onscroll = function(){scrollFunction ()};
+	function scrollFunction(){
+		if(document.body.scrollTop>500 || document.documentElement.scrollTop >500){
+			document.getElementById("button-top").style.display = "block";
+		}
+		else {
+			document.getElementById("button-top").style.display = "none";
+		}
+	}
