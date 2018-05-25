468c8d9e2543cc1018ff23428e40e775ccd19db0
Phuong Tran Ha <thaphuong@tma.com.vn>
2018-05-25 17:59:29 +0700

    edit 25/5

--- js/re-home.js
+++ js/re-home.js
@@ -1,9 +1,27 @@
 window.onscroll = function(){scrollFunction ()};
 	function scrollFunction(){
-		if(document.body.scrollTop>500 || document.documentElement.scrollTop >500){
+		if(document.body.scrollTop>400 || document.documentElement.scrollTop >400){
 			document.getElementById("button-top").style.display = "block";
 		}
 		else {
 			document.getElementById("button-top").style.display = "none";
 		}
 	}
+	document.getElementById("button-top").addEventListener("click", function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop:0}, '300');
+	});
+
+	document.getElementById("scroll-AB").addEventListener("click", function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop:(1300)}, '300');
+	});
+	document.getElementById("scroll-MP").addEventListener("click", function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop:(2000)}, '300');
+	});
+
+	document.getElementById("scroll-CM").addEventListener("click", function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop:(2700)}, '300');
+	});
